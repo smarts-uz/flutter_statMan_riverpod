e7ea8b49380b483226401493c2c00e796c3ba65f
Mukhtor03 <muxtoreshimov11@gmail.com>
2023-08-26 15:13:25 +0500

    StackOverflow example app

--- lib/main.dart
+++ lib/main.dart
@@ -1,252 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import './todo.dart';
+import 'common.dart';
+import 'home.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-final addTodoKey = UniqueKey();
-final activeFilterKey = UniqueKey();
-final completedFilterKey = UniqueKey();
-final allFilterKey = UniqueKey();
-final todoListProvider = StateNotifierProvider<TodoList, List<Todo>>((ref) {
-  return TodoList(const [
-    Todo(id: 'todo-0', description: 'hi'),
-    Todo(id: 'todo-1', description: 'hello'),
-    Todo(id: 'todo-2', description: 'bonjour'),
-  ]);
-});
-
-enum TodoListFilter {
-  all,
-  active,
-  completed,
-}
-
-final todoListFilter = StateProvider((_) => TodoListFilter.all);
-final uncompletedTodosCount = Provider<int>((ref) {
-  return ref.watch(todoListProvider).where((todo) => !todo.completed).length;
-});
-
-final filteredTodos = Provider<List<Todo>>((ref) {
-  final filter = ref.watch(todoListFilter);
-  final todos = ref.watch(todoListProvider);
-
-  switch (filter) {
-    case TodoListFilter.completed:
-      return todos.where((todo) => todo.completed).toList();
-    case TodoListFilter.active:
-      return todos.where((todo) => !todo.completed).toList();
-    case TodoListFilter.all:
-      return todos;
-  }
-});
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home(),
-    );
-  }
-}
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFF2d2d2d),
+      ),
+      builder: (context, child) {
+        final theme = Theme.of(context);
 
-class Home extends HookConsumerWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final todos = ref.watch(filteredTodos);
-    final newTodoController = useTextEditingController();
-
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Todos'),
-        ),
-        body: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-          children: [
-            TextField(
-              key: addTodoKey,
-              controller: newTodoController,
-              decoration: const InputDecoration(
-                labelText: 'What needs to be done?',
-              ),
-              onSubmitted: (value) {
-                ref.read(todoListProvider.notifier).add(value);
-                newTodoController.clear();
-              },
-            ),
-            const SizedBox(height: 42),
-            const Toolbar(),
-            if (todos.isNotEmpty) const Divider(height: 0),
-            for (var i = 0; i < todos.length; i++) ...[
-              if (i > 0) const Divider(height: 0),
-              Dismissible(
-                key: ValueKey(todos[i].id),
-                onDismissed: (_) {
-                  ref.read(todoListProvider.notifier).remove(todos[i]);
-                },
-                child: ProviderScope(
-                  overrides: [
-                    _currentTodo.overrideWithValue(todos[i]),
-                  ],
-                  child: const TodoItem(),
-                ),
-              ),
-            ],
+        return ProviderScope(
+          overrides: [
+            themeProvider.overrideWithValue(theme),
           ],
-        ),
-      ),
+          child: ListTileTheme(
+            textColor: const Color(0xFFe7e8eb),
+            child: child!,
+          ),
+        );
+      },
+      home: const MyHomePage(),
     );
   }
-}
-
-class Toolbar extends HookConsumerWidget {
-  const Toolbar({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final filter = ref.watch(todoListFilter);
-
-    Color? textColorFor(TodoListFilter value) {
-      return filter == value ? Colors.blue : Colors.black;
-    }
-
-    return Material(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: Text(
-              '${ref.watch(uncompletedTodosCount)} items left',
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-          Tooltip(
-            key: allFilterKey,
-            message: 'All todos',
-            child: TextButton(
-              onPressed: () =>
-                  ref.read(todoListFilter.notifier).state = TodoListFilter.all,
-              style: ButtonStyle(
-                visualDensity: VisualDensity.compact,
-                foregroundColor:
-                    MaterialStateProperty.all(textColorFor(TodoListFilter.all)),
-              ),
-              child: const Text('All'),
-            ),
-          ),
-          Tooltip(
-            key: activeFilterKey,
-            message: 'Only uncompleted todos',
-            child: TextButton(
-              onPressed: () => ref.read(todoListFilter.notifier).state =
-                  TodoListFilter.active,
-              style: ButtonStyle(
-                visualDensity: VisualDensity.compact,
-                foregroundColor: MaterialStateProperty.all(
-                  textColorFor(TodoListFilter.active),
-                ),
-              ),
-              child: const Text('Active'),
-            ),
-          ),
-          Tooltip(
-            key: completedFilterKey,
-            message: 'Only completed todos',
-            child: TextButton(
-              onPressed: () => ref.read(todoListFilter.notifier).state =
-                  TodoListFilter.completed,
-              style: ButtonStyle(
-                visualDensity: VisualDensity.compact,
-                foregroundColor: MaterialStateProperty.all(
-                  textColorFor(TodoListFilter.completed),
-                ),
-              ),
-              child: const Text('Completed'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-final _currentTodo = Provider<Todo>((ref) => throw UnimplementedError());
-
-class TodoItem extends HookConsumerWidget {
-  const TodoItem({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final todo = ref.watch(_currentTodo);
-    final itemFocusNode = useFocusNode();
-    final itemIsFocused = useIsFocused(itemFocusNode);
-
-    final textEditingController = useTextEditingController();
-    final textFieldFocusNode = useFocusNode();
-
-    return Material(
-      color: Colors.white,
-      elevation: 6,
-      child: Focus(
-        focusNode: itemFocusNode,
-        onFocusChange: (focused) {
-          if (focused) {
-            textEditingController.text = todo.description;
-          } else {
-            ref
-                .read(todoListProvider.notifier)
-                .edit(id: todo.id, description: textEditingController.text);
-          }
-        },
-        child: ListTile(
-          onTap: () {
-            itemFocusNode.requestFocus();
-            textFieldFocusNode.requestFocus();
-          },
-          leading: Checkbox(
-            value: todo.completed,
-            onChanged: (value) =>
-                ref.read(todoListProvider.notifier).toggle(todo.id),
-          ),
-          title: itemIsFocused
-              ? TextField(
-                  autofocus: true,
-                  focusNode: textFieldFocusNode,
-                  controller: textEditingController,
-                )
-              : Text(todo.description),
-        ),
-      ),
-    );
-  }
-}
-
-bool useIsFocused(FocusNode node) {
-  final isFocused = useState(node.hasFocus);
-
-  useEffect(
-    () {
-      void listener() {
-        isFocused.value = node.hasFocus;
-      }
-
-      node.addListener(listener);
-      return () => node.removeListener(listener);
-    },
-    [node],
-  );
-
-  return isFocused.value;
 }
